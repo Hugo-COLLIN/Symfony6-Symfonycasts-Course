36302df6519fd92a50ca7c65f43acda0895b73a5
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-17 22:35:03 +0100

    Grafikart 2 min

---
+++ projet-de-test2/src/Controller/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+    public function index() : Response
+    {
+        return new Response("C'est ok !!!");
+    }
+}
