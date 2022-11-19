c629da3b66fa5e059907353f16c961994c5f0a11
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-20 00:20:09 +0100

    C15 Twig Service & Profiler for API Requests

--- projet-de-test2/src/Controller/VinylController.php
+++ projet-de-test2/src/Controller/VinylController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage() : Response
+    public function homepage(Environment $twig) : Response
     {
         $tracks1 = [
             'Gangsta\'s paradise - Coolio',
@@ -25,11 +26,19 @@ class VinylController extends AbstractController
         //dd($tracks2);
         //dump($tracks2);
         //dump();
-        return $this->render('vinyl/homepage.html.twig', [
+        /*return $this->render('vinyl/homepage.html.twig', [
+            'title' => "Music App",
+            'tracks1' => $tracks1,
+            'tracks2' => $tracks2,
+        ]);*/
+
+        $html = $twig->render('vinyl/homepage.html.twig', [
             'title' => "Music App",
             'tracks1' => $tracks1,
             'tracks2' => $tracks2,
         ]);
+
+        return new Response($html);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
