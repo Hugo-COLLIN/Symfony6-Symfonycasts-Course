59fa8349e9c997a3801de18906039e258b7ce685
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-19 18:49:55 +0100

    C7 Twig

--- projet-de-test2/src/Controller/VinylController.php
+++ projet-de-test2/src/Controller/VinylController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage() : Response
     {
-        return new Response("Ca marche !");
+        $tracks1 = [
+            'Gangsta\'s paradise - Coolio',
+            'Waterfalls - TLC',
+        ];
+
+        $tracks2 = [
+            ["title" => 'Gangsta\'s paradise', "artist" => 'Coolio'],
+            ["title" => 'Waterfalls', "artist" => 'TLC'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => "Music App",
+            'tracks1' => $tracks1,
+            'tracks2' => $tracks2,
+        ]);
     }
 
     #[Route('/browse/{genre}')]
